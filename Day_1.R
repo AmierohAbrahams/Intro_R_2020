09feb58f4f4e0080382f5b5cbe68af6fdc4226ea
AmierohAbrahams <3559244@myuwc.ac.za>
2020-01-28 12:43:01 +0200

    First commit

---
+++ Day_1.R
@@ -0,0 +1,41 @@
+# Day 1
+# Data, laminaria, statistical analuses
+# 28 January 2020
+
+# Loading packages
+library(tidyverse)
+
+laminaria <- read_csv("data/laminaria.csv")
+
+## Viewing the data 
+
+head(laminaria) # First 6 rows
+tail(laminaria) # Last 6 rows
+glimpse(laminaria)
+view(laminaria) # OPens the data 
+names(laminaria) # column names
+
+# Tidyverse
+
+lam_sub <- laminaria %>% # Tell R which dataframe we are using
+  select(site, total_length) # Select only specific columns
+
+lam_slice <- laminaria %>% 
+  select(site, total_length) %>% # Select specific columns first
+  slice(56:78)
+
+lam_kom <- laminaria %>%
+  filter(site == "Kommetjie")
+
+
+laminaria %>% # Tell R which dataset to use
+  filter(site == "Kommetjie") %>% # Filter out only records from Kommetjie
+  nrow() # Count the number of remaining rows
+
+
+
+
+
+
+
+
