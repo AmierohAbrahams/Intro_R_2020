81b32c0357a006c182c6a25954a3282b965296a6
AmierohAbrahams <3559244@myuwc.ac.za>
2020-01-31 07:46:28 +0200

    update

--- Day_3.R
+++ Day_3.R
@@ -89,12 +89,32 @@ ggplot() +
   borders() + # The global shape file
   coord_equal() 
 
+sa_1 <- ggplot() +
+  borders(fill = "grey70", colour = "black") +
+  coord_equal(xlim = c(12, 36), ylim = c(-38, -22), expand = 0) # Force lon/lat extent
+sa_1
+
+sa_2 <- sa_1 +
+  annotate("text", label = "Atlantic\nOcean", 
+           x = 15.1, y = -32.0, 
+           size = 5.0, 
+           angle = 30, 
+           colour = "navy") +
+  annotate("text", label = "Indian\nOcean", 
+           x = 33.2, y = -34.2, 
+           size = 18.0, 
+           angle = 60, 
+           colour = "red")
+sa_2
 
 
-
-
-
-
+sa_3 <- sa_2 +
+  # scalebar(x.min = 22, x.max = 26, y.min = -36, y.max = -35, # Set location of bar
+  #          dist = 200, height = 1, st.dist = 0.8, st.size = 4, # Set particulars
+  #          transform = TRUE, model = "WGS84") + # Set appearance
+  north(x.min = 22.5, x.max = 25.5, y.min = -33, y.max = -31, # Set location of symbol
+        scale = 1.2, symbol = 16)
+sa_3
 
 
 
